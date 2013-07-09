a2302085ee6daaad5cc5088a55949d59b95c941a
angga adiputra <anggaadiputra@ubuntu.ubuntu-domain>
2013-07-09 17:40:17 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streaming::Application.config.secret_token = '80ef33962fdb93ba169cfef6cf0aa4dcc3d0065c6a64d7b02fe915116558b60d4ea84a0b37e72cb786619ba246353494989b75a33e3a42b618aead5381aaa78b'
